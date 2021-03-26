5580aba348218cb1d41e1926e97f7ac5e55d12ab
jsandhu <jsandhu@pnimedia.com>
2021-03-26 13:19:29 -0500

    added wix

--- lib/dynamic-images.d.ts
+++ lib/dynamic-images.d.ts
@@ -9,11 +9,6 @@ export declare class DynamicImages {
     private imageDataList;
     wizardService: WizardService;
     constructor(imageDataList: ImageData[], wizardService: WizardService);
-    imageUrls: {
-        src: string;
-        text: string;
-        imageName: string;
-    }[];
     fetchProducts: (imageName: string) => Promise<void>;
     handleImageClick: (event: any) => void;
     showImages: () => void;
